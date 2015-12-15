529d3d397fee9819acb57eb67b1c85153a0ca9ad
Simon Knight <simknigh@cisco.com>
2015-12-16 00:24:34 +1030

    Bump version: 0.21.2 → 0.21.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setup (
      name = "autonetkit",
-     version = "0.21.2",
+     version = "0.21.3",
      description = 'Automatic configuration generation for emulated networks',
      long_description = 'Automatic configuration generation for emulated networks',
 
