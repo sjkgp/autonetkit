ca06a1431f874c3edb75028340511fc1d5c674f3
Dominik Kapisinsky <dkapisin@cisco.com>
2016-03-09 16:15:45 +0000

    comparison of generated configs

--- tests/test_graphml.py
+++ tests/test_graphml.py
@@ -1,19 +1,26 @@
-import autonetkit.console_script as console_script
-import subprocess
 import os
-import sys
-#from cStringIO import StringIO
-import shutil
+import logging
+import pkg_resources
+
+import autonetkit.console_script as console_script
 import autonetkit.log as log
 import autonetkit
 import autonetkit.load.graphml as graphml
-
 
 # stdio redirect from stackoverflow.com/q/2654834
 
 #TODO: add feature that reports if only IP addresses have changed: match the diff to an IP regex
 
-def compare_output_expected(topology_name, automated = True):
+
+def resource_path(relative):
+    return pkg_resources.resource_filename(__name__, relative)
+
+
+def compare_output_expected(topology_name):
+    import difflib
+
+    message = ''
+
     log.info("Testing %s" % topology_name)
     input_filename = "%s.graphml" % topology_name
 
@@ -22,25 +29,61 @@ def compare_output_expected(topology_name, automated = True):
 
     arg_string = "-f %s --quiet --render" % input_file
     args = console_script.parse_options(arg_string)
-
     console_script.main(args)
 
-    #TODO: check the output files
+    #: set log level back to INFO because --quit it turns down to WARNING
+    log.ank_logger.setLevel(logging.INFO)
+
+    #: load expected structure
+    topology_expected_dir_name = topology_name + '_output'
+    topology_expected_dir = resource_path(topology_expected_dir_name)
+    topology_output_dir = os.path.join(os.path.dirname(os.path.dirname(autonetkit.__file__)), 'rendered')
+
+    differences_found = False
+
+    #: compare each file from expected dir to file in output directory
+    for root, dirs, files in os.walk(topology_expected_dir):
+        for file in files:
+            exp_file_path = os.path.join(root, file)
+            exp_file_relpath = os.path.relpath(exp_file_path, topology_expected_dir)
+            output_file_path  = os.path.join(topology_output_dir, exp_file_relpath)
+            with open(exp_file_path, "r") as fh:
+                expected_result = fh.read()
+            with open(output_file_path, "r") as fh:
+                output_result = fh.read()
+            expected_lines = expected_result.splitlines()
+            output_lines = output_result.splitlines()
+
+            #remove line with version and time stamp
+            if file == 'motd.txt':
+                expected_lines = expected_lines[2:]
+                output_lines = output_lines[2:]
+
+            diff = difflib.context_diff(expected_lines, output_lines, n=0)
+            diff = "\n".join(diff)
+            if len(diff) > 0:
+                differences_found = True
+                # print diff
+                diff_with_context = difflib.context_diff(
+                        expected_lines, output_lines, n=3)
+                diff_with_context = "\n".join(diff_with_context)
+                message += " Change in config %s: %s" % (
+                    exp_file_relpath, diff_with_context)
+                log.warning(message)
+            else:
+                log.info("Verified: %s", exp_file_relpath)
+
+    if differences_found:
+        raise AssertionError("Failed: %s" % topology_name)
+
+    log.info("Verified topology %s" % topology_name)
+
 
 topologies = [
-"small_internet",
-"Aarnet",
-"multigraph",
+    "small_internet",
+    "Aarnet",
+    "multigraph",
 ]
-
-
-automated = True # whether to open ksdiff, log to file...
-if __name__ == "__main__":
-    automated = False
-
-for topology in topologies:
-    print "Testing topology", topology
-    compare_output_expected(topology, automated = automated)
 
 
 # special case testing
@@ -58,6 +101,7 @@ def build_anm(topology_name):
     anm = applicator.design()
     return anm
 
+
 def test():
     anm = build_anm("blank_labels")
     actual_labels = sorted(anm['phy'].nodes())
@@ -74,3 +118,16 @@ def test():
     actual_asns = [n.asn for n in anm['phy']]
     expected_asns = [1, 1, 1]
     assert(actual_asns == expected_asns)
+
+
+def test_topologies():
+    for topology in topologies:
+        yield compare_output_expected, topology
+
+
+automated = True # whether to open ksdiff, log to file... currently not used
+if __name__ == "__main__":
+    automated = False
+    for topology in topologies:
+        print "Testing topology", topology
+        compare_output_expected(topology)
